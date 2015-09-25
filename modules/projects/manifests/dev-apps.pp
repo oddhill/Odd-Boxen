3e77f1950b86779f4c8f76ff3f0cb26c1f0fe1a5
Jens Lind <jens.lind@oddhill.se>
2015-09-25 15:44:02 +0200

    Removed tower puppet

--- modules/projects/manifests/dev-apps.pp
+++ modules/projects/manifests/dev-apps.pp
@@ -2,5 +2,5 @@ class projects::dev-apps {
   # Install some common-apps
   include brewcask
   package { 'sequel-pro': provider => 'brewcask' }
-  include tower
+  package { 'tower': provider => 'brewcask' }
 }
