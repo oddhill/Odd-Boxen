e49736dfd0c7e791b5a0a19a5a73284922a4f4ce
Jens Lind <jens.lind@oddhill.se>
2015-09-28 10:02:33 +0200

    Install ruby gems for 2.1.2

--- modules/oddhill/manifests/implementation/ruby_gems.pp
+++ modules/oddhill/manifests/implementation/ruby_gems.pp
@@ -1,23 +1,25 @@
 class oddhill::implementation::ruby_gems {
+  $version = '2.1.2'
+
   ruby_gem { 'sass':
-    gem     => 'sass',
-    version => '3.4.14',
-    ruby_version    => '2.1.1'
+    gem             => 'sass',
+    version         => '3.4.14',
+    ruby_version    => $version
   }
 
   ruby_gem { 'compass':
-    gem     => 'compass',
-    ruby_version    => '2.1.1'
+    gem             => 'compass',
+    ruby_version    => $version
   }
 
   ruby_gem { 'scss-lint':
-    gem     => 'scss-lint',
-    ruby_version    => '2.1.1'
+    gem             => 'scss-lint',
+    ruby_version    => $version
   }
 
   ruby_gem { 'sass-globbing':
-    gem     => 'sass-globbing',
-    version => '1.1.0',
-    ruby_version    => '2.1.1'
+    gem             => 'sass-globbing',
+    version         => '1.1.0',
+    ruby_version    => $version
   }
 }
